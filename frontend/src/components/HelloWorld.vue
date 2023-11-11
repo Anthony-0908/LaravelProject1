ca090b97778032f3deb39554f8738a2be1ee9a98
jaylordcantor <jaylordcantor@gmail.com>
2023-11-11 10:00:48 +0800

    first commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card class="text-center bg-black" >
+    <v-btn color="">
+    <v-icon dense color="error">mdi-email</v-icon>
+    <span class="text-green"> Clickasdf </span>
+</v-btn>
+  </v-card>
+
+</template>
+
+<script setup lang="ts">
+  //
+</script>
